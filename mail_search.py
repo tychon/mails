e4376e1700b769e2d6f862d0a383b86351b04c22
tychon <tychon4.2@googlemail.com>
2013-12-22 20:10:02 +0100

    support search by date

--- mail_search.py
+++ mail_search.py
@@ -1,10 +1,11 @@
-
+#!/usr/bin/python
 import sys, logging
 import requests, json
 
 import common
 import config
 
+# Returns a list of documents matching the search criteria.
 # May return an empty list, but not None.
 #TODO support doc.keywords
 def search(hfrom=None, hto=None, since=None, before=None, labels=[], logger='none'):
@@ -47,7 +48,59 @@ def search(hfrom=None, hto=None, since=None, before=None, labels=[], logger='non
       return []
     log.info("%s results for To: %s*", len(res), hto)
   
-  #TODO filter by date
+  # search by date
+  if before: beforecmp = before + '\u9999'
+  if since and before:
+    if res:
+      res = filter(lambda doc: doc.get('date', '\0') >= since and doc.get('date', '\u9999') <= beforecmp, res)
+    else:
+      r = requests.get(config.couchdb_url+config.design_doc
+               +('_view/date?startkey="%s"&endkey="%s\u9999"' % (since, before))
+              , auth=config.couchdb_auth, verify=False)
+      log.info((r.status_code, r.url))
+      if not r.status_code == 200:
+        raise IOError("Query failed, code %d:\n  %s\n  %s" % (r.status_code, r.url, r.text))
+      res = json.loads(r.text)['rows']
+      res = map(lambda x: x['value'], res)
+    
+    if len(res) == 0:
+      log.info("No results for %s <= date <= %s", since, before)
+      return []
+    log.info("%s results for %s <= date <= %s", len(res), since, before)
+  elif since:
+    if res:
+      res = filter(lambda doc: doc.get('date', '\0') >= since, res)
+    else:
+      r = requests.get(config.couchdb_url+config.design_doc
+               +('_view/date?startkey="%s"' % since)
+              , auth=config.couchdb_auth, verify=False)
+      log.info((r.status_code, r.url))
+      if not r.status_code == 200:
+        raise IOError("Query failed, code %d:\n  %s\n  %s" % (r.status_code, r.url, r.text))
+      res = json.loads(r.text)['rows']
+      res = map(lambda x: x['value'], res)
+    
+    if len(res) == 0:
+      log.info("No results for %s <= date", since)
+      return []
+    log.info("%s results for %s <= date", len(res), since)
+  elif before:
+    if res:
+      res = filter(lambda doc: doc.get('date', '\u9999') <= beforecmp, res)
+    else:
+      r = requests.get(config.couchdb_url+config.design_doc
+               +('_view/date?endkey="%s\u9999"' % before)
+              , auth=config.couchdb_auth, verify=False)
+      log.info((r.status_code, r.url))
+      if not r.status_code == 200:
+        raise IOError("Query failed, code %d:\n  %s\n  %s" % (r.status_code, r.url, r.text))
+      res = json.loads(r.text)['rows']
+      res = map(lambda x: x['value'], res)
+    
+    if len(res) == 0:
+      log.info("No results for date <= %s", before)
+      return []
+    log.info("%s results for date <= %s", len(res), before)
   
   # search labels
   if labels and len(labels):
@@ -74,10 +127,10 @@ def search(hfrom=None, hto=None, since=None, before=None, labels=[], logger='non
   return res
 
 def main():
+  sys.stderr.write(repr(sys.argv))
   # parse args
-  hto = None
-  hfrom = None
-  hdate = None
+  hfrom = hto = None
+  since = before = None
   labels = None
   i = 1
   while i < len(sys.argv):
@@ -91,11 +144,17 @@ def main():
     elif arg == "-labels":
       i += 1
       labels = sys.argv[i].split(',')
+    elif arg == "-since":
+      i += 1
+      since = sys.argv[i]
+    elif arg == "-before":
+      i += 1
+      before = sys.argv[i]
     else:
-      err("unknown arg %s" % arg)
+      common.err("unknown arg %s" % arg)
       sys.exit(1)
     i += 1
-  res = search(hfrom, hto, labels=labels, logger='stderr')
+  res = search(hfrom, hto, since, before, labels, logger='stderr')
   for val in res: print val['_id']
 
 if __name__ == '__main__': main()
