6f15dc87d5b68ff7a0f957aed52b519e979dbd1f
tychon <tychon4.2@googlemail.com>
2013-12-22 22:39:09 +0100

    function for downloading and saving mails from CouchDB

--- mail_download.py
+++ mail_download.py
@@ -1,62 +1,78 @@
+#!/usr/bin/python
+# NOTE: --exec args are run with shell=True, this is risky if you use
+# untrusted programms!
 
 import sys, os
+import common, logging
 import mailbox
 import re
 import requests
 import subprocess
 
-from common import info, err
 import config
 
-# open mailbox and read args
-box = None
-append = True
-execute = None
-
-i = 1
-while i < len(sys.argv):
-  arg = sys.argv[i]
-  if arg == '--override':
-    append = False
-  elif arg == '--mbox':
-    if box: err("Multiple mailboxes given.")
-    i += 1
-    try:
-      if not append: os.remove(sys.argv[i])
-    except OSError: pass
-    box = mailbox.mbox(sys.argv[i])
-  elif arg == '--maildir':
-    if box: err("Multiple mailboxes given.")
-    i += 1
-    box = mailbox.Maildir(sys.argv[i])
-  elif arg == '--exec':
-    i += 1
-    execute = sys.argv[i]
-  i += 1
-
-# download messages and add them to mailbox
-re_id = re.compile('([0-9A-Fa-f]+)\s+')
-for count, line in enumerate(sys.stdin):
-  mo = re_id.match(line)
-  if not mo:
-    err("Ignoring line %d: %s" % (count, line))
-    continue
-  docid = mo.group(1)
+def download(docid=None, box=None, cmd=None, stream=None, logger='none'):
+  log = logging.getLogger(logger)
   r = requests.get(config.couchdb_url+docid+'/mail'
           , auth=config.couchdb_auth, verify=False)
-  info((r.status_code, r.url))
+  log.info((r.status_code, r.url))
   mail = r.text.encode('utf8')
   if r.status_code == 200:
-    if execute != None:
-      process = subprocess.Popen(execute, stdin=subprocess.PIPE, shell=True)
+    if cmd != None:
+      process = subprocess.Popen(cmd, stdin=subprocess.PIPE, shell=True)
       process.communicate(mail)
-      info("exec return code %d" % process.wait())
+      log.info("%s\n  returned with code %d" % (cmd, process.wait()))
     if box != None:
       key = box.add(mail)
-      info("Saved under key %s" % key)
-    if execute == None and box == None: print r.text
+      log.info("Mail saved under key %s" % key)
+    if stream != None:
+      stream.write(mail)
   else:
-    info("WARNING: status code: %d, text: %s" % (r.status_code, r.text))
+    raise IOError("Request failed, status code: %d\n  URL %s\n  RESP: %s" % (r.status_code, r.url, r.text))
+  return r.text
 
-if isinstance(box, mailbox.mbox): box.close()
+def main():
+  # open mailbox and read args
+  box = cmd = None
+  append = True
+  
+  i = 1
+  while i < len(sys.argv):
+    arg = sys.argv[i]
+    if arg == '--override':
+      if i > 1: common.err("You should give --override as first argument!")
+      append = False
+    elif arg == '--mbox':
+      if box: err("Multiple mailboxes given.")
+      i += 1
+      try:
+        if not append: os.remove(sys.argv[i])
+      except OSError: pass
+      box = mailbox.mbox(sys.argv[i])
+    elif arg == '--maildir':
+      if box: err("Multiple mailboxes given.")
+      i += 1
+      box = mailbox.Maildir(sys.argv[i])
+    elif arg == '--exec':
+      i += 1
+      cmd = sys.argv[i]
+    i += 1
+  
+  re_id = re.compile('([0-9A-Fa-f]+)\s+')
+  for count, line in enumerate(sys.stdin):
+    mo = re_id.match(line)
+    if not mo:
+      common.info("Ignoring line %d: %s" % (count, line))
+      continue
+    docid = mo.group(1)
+    if box == None and not cmd: stream = sys.stdout
+    else: stream = None
+    try:
+      download(docid, box, cmd, stream, 'stderr')
+    except IOError as e:
+      common.info(str(e))
+  
+  if isinstance(box, mailbox.mbox): box.close()
+
+if __name__ == '__main__': main()
 
