1c41a49bd55c5ccb3eac4e1e7c0e7f5092c479ec
tychon <tychon4.2@googlemail.com>
2013-12-26 19:40:39 +0100

    rewrite mail_upload as function

--- mail_upload.py
+++ mail_upload.py
@@ -6,171 +6,76 @@
 # Writes log output to config.errorlog
 # Saves mail in case of failed upload to config.backupdir (see error log for more info)
 
-import sys
-import logging
-import traceback
-import os
-import random
-import email.parser
-import email.message
-import hashlib
-import time
-import json
-import requests
+import sys, os, random
+import common, logging
+import email.parser, email.message
+import hashlib, time
+import requests, json
 
-import common
 import parseaddr
 # Import information about couchdb 
 import config
 
-# TODO Use logging setup from common.py
-# Setup logging
-# For possible format strings see LogRecord attributes in logging module doc
-# You have to set the level to DEBUG to see any of the .info messages.
-logging.basicConfig(filename=config.errorlog, format='%(asctime)s %(levelname)s: %(message)s')
-log = logging.getLogger()
+class MailParserException(Exception):
+  def __init__(self, value):
+    self.value = value
+  def __str__(self):
+    return repr(self.value)
 
-def logexception(type, value, tb):
-  msg = "Exception, possible loss of data!\n %s" % ' '.join(traceback.format_exception(type, value, tb, limit=100))
-  log.critical(msg)
-  logging.shutdown()
-  sys.exit(1)
-sys.excepthook = logexception
-
-# Read mail from stdin
-mail = sys.stdin.read()
-if len(mail) == 0:
-  log.error("Empty mail")
-  logging.shutdown()
-  sys.exit(1)
-
-logging.info("Mail received.") # not printed, unless you set logging level to debug
-
-# hash mail for document id
-sha = hashlib.new('sha256')
-sha.update(mail)
-hexdigest = sha.hexdigest()
-
-# helper function to save mail to a file in case of an error
-def save_mail():
-  randomness = ''.join(random.choice('abcdef0123456789') for x in range(10))
-  filename = os.path.expanduser("%s%s_%s" % (config.backupdir, hexdigest[:10], randomness))
-  f = open(filename, 'w+')
-  f.write(mail)
-  f.close()
-  log.error("Written mail %s\n  to %s", hexdigest, filename)
-
-# parse command line args
-override = False
-for arg in sys.argv:
-  if arg == "--override":
-    override = True
-
-# parse mail
-message = email.parser.Parser().parsestr(mail)
-
-# test defects and try to save defect mails
-if len(message.defects) != 0:
-  log.error("Parser signaled defect in mail %s:\n  %s\n  trying to save mail" % (hexdigest, str(message.defects)))
-  save_mail()
-  logging.shutdown()
-  sys.exit(1)
-
-if not message.get('From', None) \
-or not message.get('To', None) \
-or not message.get('Subject', None) \
-or not message.get('Date', None) :
-  log.error("Missing one or more fields of From, To, Subject and Date\n  mail hash: %s\n  trying to save mail", hexdigest)
-  save_mail()
-  logging.shutdown()
-  sys.exit(1)
-
-# TODO lowercase email addresses
-froms = tos = None
-try:
-  froms = common.uniquify(parseaddr.extract_addresses(parseaddr.parse_address_list(message.get('From'))))
-except parseaddr.ParserException as e:
-  log.error("Couldn't parse address in From field list.\n  %s\n  From: %s\n  trying to save mail", str(e), message.get('From'))
-  save_mail()
-  logging.shutdown()
-  sys.exit(1)
-try:
-  tos = common.uniquify(parseaddr.extract_addresses(parseaddr.parse_address_list(message.get('To'))))
-except parseaddr.ParserException as e:
-  log.error("Couldn't parse address in To field list.\n  %s\n  To: %s\n  trying to save mail", str(e), message.get('To'))
-  save_mail()
-  logging.shutdown()
-  sys.exit(1)
-
-# parse mail send time and convert it to UTC
-sendtimestr = common.convert_from_maildate(message.get('Date'))
-
-# format current UTC time
-nowtimestr = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(time.time()))
-
-#TODO check gpg signed and encrypted (not verification) in extra module
-#TODO auto labeling in extra module
-#TODO create index for full text search
-
-data = {
-  'upload_date': nowtimestr
-, 'flags': []
-, 'labels': ['unread'] # TODO do this in autolabeling
-, 'keywords': []
-, 'from': froms
-, 'to': tos
-, 'date': sendtimestr
-, 'subject': message.get('Subject')
-}
-
-# Retrieve rev if override is enabled
-oldrevision = ''
-if override:
-  response = requests.head(config.couchdb_url+hexdigest
-      , auth=config.couchdb_auth, verify=False)
-  print 'HEAD', response.url
-  print response.status_code
-  print response.headers
-  rev = json.loads(response.headers.get('etag', '""'))
-  if rev != '':
-    oldrevision = '?rev='+rev
-    print "Overriding old mail!"
-  print
-
-# UPLOAD document
-# verify=False is for untrusted SSL certificates (you created on your own)
-response = requests.put(config.couchdb_url+hexdigest+oldrevision
-    , auth=config.couchdb_auth, verify=False, data=json.dumps(data))
-
-print 'PUT', response.url
-print response.status_code
-print response.text
-respjson = json.loads(response.text)
-if not respjson.get('ok', False):
-  log.error("Could not upload metadata\n  hash: %s,\n  CouchDB response code %d, text: %s  trying to save mail" % (hexdigest, response.status_code, response.text))
-  save_mail()
-  logging.shutdown()
-  sys.exit(1)
-print
-
-# UPLOAD original mail as attachment
-response = requests.put(config.couchdb_url+hexdigest+'/mail?rev='+respjson.get('rev')
-    , headers={'content-type':'text/plain'}
-    , auth=config.couchdb_auth, verify=False, data=mail)
-
-print 'PUT', response.url
-print response.status_code
-print response.text
-respjson = json.loads(response.text)
-if not respjson.get('ok', False):
-  log.error("Could not upload mail attachment\n  There is a mail without original message in your couchdb!\n  hash: %s,\n  CouchDB response code %d, text: %s  trying to save mail" % (hexdigest, response.status_code, response.text))
-  save_mail()
-  logging.shutdown()
-  sys.exit(1)
-
-# Use ?attachments=true to GET attachments with documents base64 encoded in one request.
-
-# everything is ok. exit.
-logging.shutdown()
-sys.exit(0)
+def parsemail(mail, logger='none'):
+  log = logging.getLogger(logger)
+  if len(mail) == 0:
+    raise MailParserException('Empty mail')
+  log.info("Mail received.")
+  
+  data = {
+    'type': 'mail'
+  , 'mail': mail
+  }
+  
+  # hash mail for document id
+  sha = hashlib.new('sha256')
+  sha.update(mail)
+  hexdigest = sha.hexdigest()
+  
+  # parse mail
+  message = email.parser.Parser().parsestr(mail)
+  
+  # test defects and try to save defect mails
+  if len(message.defects) != 0:
+    raise MailParserException("Parser signaled defect in mail %s:\n  %s" % (hexdigest, str(message.defects)))
+  
+  if message.get('From', None):
+    try:
+      data['from'] = froms = common.uniquify(parseaddr.extract_addresses(parseaddr.parse_address_list(message.get('From'))))
+    except parseaddr.ParserException as e:
+      raise MailParserException("Couldn't parse address in From field list.\n  %s\n  From: %s" % (str(e), message.get('From')))
+  
+  if message.get('To', None):
+    try:
+      data['to'] = common.uniquify(parseaddr.extract_addresses(parseaddr.parse_address_list(message.get('To'))))
+    except parseaddr.ParserException as e:
+      raise MailParserException("Couldn't parse address in To field list.\n  %s\n  To: %s" % (str(e), message.get('To')))
+  
+  if message.get('Date', None):
+    try:
+      # parse mail send time and convert it to UTC
+      data['date'] = common.convert_from_maildate(message.get('Date'))
+    except parseaddr.ParserException as e:
+      raise MailParserException("Could not convert %s to YYYY-MM-DD hh:mm:ss\n  %s" % (message.get('Date'), str(e)))
+  
+  # format current UTC time
+  data['upload_date'] = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(time.time()))
+  
+  data['labels'] = []
+  if message.get('Status', None):
+   if not 'R' in message.get('Status'):
+     data['labels'].append('unread')
+  else: data['labels'].append('unread')
+  
+  #TODO check gpg signed and encrypted (no verification) in extra module
+  #TODO auto labeling in extra module
+  #TODO create index for full text search
+  
+  return data
 
