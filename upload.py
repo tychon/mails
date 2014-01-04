5eaae65607ac3e6708ee33331af843dadd35b04d
tychon <tychon4.2@googlemail.com>
2014-01-04 23:25:37 +0100

    do some elaborate logging on upload

--- upload.py
+++ upload.py
@@ -39,12 +39,14 @@ def parsemail(mail, logger='none'):
   if message.get('From', None):
     try:
       data['from'] = froms = common.uniquify(parseaddr.extract_addresses(parseaddr.parse_address_list(message.get('From'))))
+      log.info("From: %s", ' '.join(froms))
     except parseaddr.AddrParserException as e:
       raise MailParserException("Couldn't parse address in From field list.\n  %s\n  From: %s" % (str(e), message.get('From')))
   
   if message.get('To', None):
     try:
-      data['to'] = common.uniquify(parseaddr.extract_addresses(parseaddr.parse_address_list(message.get('To'))))
+      data['to'] = tos = common.uniquify(parseaddr.extract_addresses(parseaddr.parse_address_list(message.get('To'))))
+      log.info("To: %s", ' '.join(tos))
     except parseaddr.AddrParserException as e:
       raise MailParserException("Couldn't parse address in To field list.\n  %s\n  To: %s" % (str(e), message.get('To')))
   
@@ -52,11 +54,13 @@ def parsemail(mail, logger='none'):
     try:
       # parse mail send time and convert it to UTC
       data['date'] = common.convert_from_maildate(message.get('Date'))
+      log.info("Date: %s", data['date'])
     except Exception as e:
       raise MailParserException("Could not convert %s to YYYY-MM-DD hh:mm:ss\n  %s" % (message.get('Date'), str(e)))
   
   # format current UTC time
   data['upload_date'] = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(time.time()))
+  log.info("upload-date: %s", data['upload_date'])
   
   data['labels'] = []
   if message.get('Status', None):
@@ -64,8 +68,10 @@ def parsemail(mail, logger='none'):
      data['labels'].append('unread')
   else: data['labels'].append('unread')
   
-  #TODO check gpg signed and encrypted (no verification) in extra module
   #TODO auto labeling in extra module
+  log.info("Labels: %s", ' '.join(data['labels']))
+  
+  #TODO check gpg signed and encrypted (no verification) in extra module
   
   return data
 
@@ -104,13 +110,14 @@ def upload(hexdigest, metadata, mail, override=False, logger='none'):
     raise IOError("Could not upload mail attachment\n  There is a mail without original message in your couchdb!\n  hash: %s,\n  CouchDB response code %d, text: %s" % (hexdigest, r.status_code, r.text))
 
 def main():
-  log = logging.getLogger('upload')
+  ilog = logging.getLogger('stderr')
+  elog = logging.getLogger('upload')
   mail = sys.stdin.read()
   # hash mail for document id
   sha = hashlib.new('sha256')
   sha.update(mail)
   hexdigest = sha.hexdigest()
-  log.info("Mail received: %s", hexdigest)
+  ilog.info("Mail received: %s", hexdigest)
   
   # helper function to save mail to a file in case of an error
   def save_mail():
@@ -119,13 +126,13 @@ def main():
     f = open(filename, 'w+')
     f.write(mail)
     f.close()
-    log.error("Written mail %s\n  to %s", hexdigest, filename)
+    elog.error("Written mail %s\n  to %s", hexdigest, filename)
   
   try:
-    data = parsemail(mail, logger='upload')
+    data = parsemail(mail, logger='stderr')
     upload(hexdigest, data, mail, "--override" in sys.argv, 'stderr')
   except:
-    log.error("Exception while parsing or uploading mail:\n %s" % traceback.format_exc())
+    elog.error("Exception while parsing or uploading mail:\n %s" % traceback.format_exc())
     save_mail()
     logging.shutdown()
     sys.exit(1)
